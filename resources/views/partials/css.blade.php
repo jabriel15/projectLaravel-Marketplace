e6d401dbb6a3506d409e08e6e3df2f6c49424e5c
___jb <jabrielbams@gmail.com>
2022-07-22 10:50:45 +0700

    landing view

---
+++ resources/views/partials/css.blade.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" type="text/css" href="Bootstrap/dist/css/bootstrap-reboot.css">
+<link rel="stylesheet" type="text/css" href="Bootstrap/dist/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="Bootstrap/dist/css/bootstrap-grid.css">
+
+<!-- Main Styles CSS -->
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/fonts.min.css">
